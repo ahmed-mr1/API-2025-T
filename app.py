6bb4c93158bb31259ad5b3b9a30ba08cca9c10d5
Ahmed Mrabet <mrabetahmed.tbs@gmail.com>
2025-10-29 07:45:29 +0100

    Add database - 29 oct

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:<YOUR_PASSWORD>@localhost:5432/postgres"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 specializations = [
     {
         "name": "IT",
